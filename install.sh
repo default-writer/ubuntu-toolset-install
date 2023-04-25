466afee6cf2deae9b7efa4c156b5ba101dbde5b5
default-writer <funcelot@outlook.com>
2023-04-25 11:06:02 +0300

    added libvirtd service

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 sudo apt install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
 #intalls virt-manager
 sudo apt install -y virt-manager
+#enables libvirtd
+sudo systemctl enable --now libvirtd
